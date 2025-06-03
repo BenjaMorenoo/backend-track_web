7fa9a5df0307b0bc776364cc937184b5d21632bf
BenjaMorenoo <benjaben100@gmail.com>
2025-06-02 20:38:14 -0400

    cors

--- server.js
+++ server.js
@@ -3,9 +3,19 @@ const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const usuarioRoutes = require('./routes/usuarioRoutes.js')
+//cors
+const cors = require('cors');
+
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type'] // Encabezados permitidos que sirven para las peticiones desde el frontend
+}
+
+app.use(cors(corsOptions));
 
 
-dotenv.config()
+dotenv.config();
 
 
 app.use(express.json());
@@ -23,8 +33,8 @@ mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-.then(() => console.log('MongoDB Conectado'))
-.catch((err) => console.log('Error al conectar a la BD: ', err))
+    .then(() => console.log('MongoDB Conectado'))
+    .catch((err) => console.log('Error al conectar a la BD: ', err))
 
 app.listen(3000, () => {
     console.log("Servidor escuchando en puerto: 3000")
